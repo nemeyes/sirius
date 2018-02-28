962147db2a7b1bb551c430c86260efed3f8f4a47
msjung <jms0804@gmail.com>
2018-02-28 11:42:49 +0900

    [msjung] add missing files

---
+++ app/server/arbitrator/LogSettingsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sirius.app.server.arbitrator
+{
+    public class LogSettingsViewModel
+        : NotifyPropertyChanged, IDataErrorInfo
+    {
+        private string log_root_path = "d:\\log";
+
+        public string LogRootPath
+        {
+            get { return this.log_root_path; }
+            set
+            {
+                this.log_root_path = value;
+                OnPropertyChanged("LogRootPath");
+            }
+        }     
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "LogRootPath")
+                {
+                    return string.IsNullOrEmpty(this.log_root_path) ? "Required value" : null;
+                }            
+                return null;
+            }
+        }
+    }
+}
