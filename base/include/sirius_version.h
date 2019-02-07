e07c99c666bc48310a91f7747d7fbd3b7943657a
jms0804 <jms0804@gmail.com>
2019-02-07 17:41:28 +0900

    [msjung] update version

--- base/include/sirius_version.h
+++ base/include/sirius_version.h
@@ -1,6 +1,6 @@
 //GIT-VS-VERSION-GEN.bat generated resource header.
-#define GEN_VER_VERSION_STRING   "1.0.0.35\0" 
-#define GEN_VER_DIGITAL_VERSION  1,0,0,35
+#define GEN_VER_VERSION_STRING   "1.0.0.38\0" 
+#define GEN_VER_DIGITAL_VERSION  1,0,0,38
 #define GEN_VER_VERSION_HEX      0x0001000000000000 
 #define GEN_VER_COMMENT_STRING   "Release Candidate 0\0" 
 #define GEN_VER_PRIVATE_FLAG     0 
